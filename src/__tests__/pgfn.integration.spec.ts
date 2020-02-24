f67437f130f47a06a6d869f07511711009f23b51
Benoit Lemoine <blemoine@xebia.fr>
2020-02-24 07:17:53 -0500

    feat: support for transact

--- src/__tests__/pgfn.integration.spec.ts
+++ src/__tests__/pgfn.integration.spec.ts
@@ -1,31 +1,30 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import { sql } from '../query/sql-parser';
 import { deser, sequenceDeser } from '../serde/SqlDeserializer';
 
 describe('sql-query', () => {
-  let client: Client;
+  let pool: Pool;
   beforeAll(async () => {
-    client = new Client({
+    pool = new Pool({
       connectionString: process.env.DATABASE_URL,
     });
-    await client.connect();
   });
   afterAll(() => {
-    client.end();
+    pool.end();
   });
 
   test('should be able to create tables and fill them', async () => {
-    const createCarTable = sql`CREATE TABLE car ( id SERIAL, name TEXT DEFAULT NULL, date TIMESTAMPTZ)`;
-
+    const createCarTable = sql`CREATE TABLE car ( id SERIAL, name TEXT DEFAULT NULL, date TIMESTAMPTZ)`.update();
     const carName = 'titine';
     const date = new Date('2020-02-03T05:06:07Z');
     const date2 = new Date('2021-05-15T15:16:17Z');
-    const insertCar = sql`INSERT INTO car(name, date) VALUES (${carName}, ${date}), (null, ${date2}) RETURNING *`;
-    await createCarTable.update().run(client);
+    const insertCar = sql`INSERT INTO car(name, date) VALUES (${carName}, ${date}), (null, ${date2}) RETURNING *`.list(
+      sequenceDeser(deser.toInteger, deser.toString.or(deser.toNull), deser.toDate)
+    );
 
-    const result = await insertCar
-      .list(sequenceDeser(deser.toInteger, deser.toString.or(deser.toNull), deser.toDate))
-      .run(client);
+    const cio = createCarTable.flatMap(() => insertCar);
+
+    const result = await cio.transact(pool);
 
     expect(result).toStrictEqual([
       [1, carName, date],
--- src/connection/ConnectionIO.ts
+++ src/connection/ConnectionIO.ts
@@ -1,4 +1,4 @@
-import { ClientBase } from 'pg';
+import { ClientBase, Pool } from 'pg';
 
 export class ConnectionIO<A> {
   constructor(public run: (client: ClientBase) => Promise<A>) {}
@@ -11,5 +11,19 @@ export class ConnectionIO<A> {
   flatMap<B>(mapper: (a: A) => ConnectionIO<B>): ConnectionIO<B> {
     const parentRun = this.run;
     return new ConnectionIO<B>((client) => parentRun(client).then((a) => mapper(a).run(client)));
+  }
+
+  async transact(pool: Pool): Promise<A> {
+    const client = await pool.connect();
+    try {
+      await client.query('BEGIN');
+
+      return this.run(client);
+    } catch (e) {
+      await client.query('ROLLBACK');
+      throw e;
+    } finally {
+      client.release();
+    }
   }
 }
