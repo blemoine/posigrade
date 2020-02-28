8d3850e42b69b855781007d75f47c02223bbba47
Benoit Lemoine <blemoine@xebia.fr>
2020-02-28 07:18:39 -0500

    test: add bigint deser tests

--- src/serde/SqlDeserializer.spec.ts
+++ src/serde/SqlDeserializer.spec.ts
@@ -1,5 +1,5 @@
-import { deser, sequenceDeser } from './SqlDeserializer';
-import { Success } from '../result/Result';
+import { deser, namedDeser, sequenceDeser } from './SqlDeserializer';
+import { Failure, Success } from '../result/Result';
 
 describe('SqlDeserialize', () => {
   it('should be able to combine deserializer', () => {
@@ -28,5 +28,23 @@ describe('SqlDeserialize', () => {
     const result2 = strOrNull.deserialize([null]);
 
     expect(result2).toStrictEqual(Success.of(null));
+  });
+
+  it('should be able to deserialize bigInt', () => {
+    const result = deser.toBigInt.deserialize(['1234']);
+
+    expect(result).toStrictEqual(Success.of(BigInt('1234')));
+  });
+
+  it('should be able to deserialize a named bigInt', () => {
+    const result = namedDeser.toBigInt('nb').deserialize({ nb: '1234' });
+
+    expect(result).toStrictEqual(Success.of(BigInt('1234')));
+  });
+
+  it('should fail to convert an invalid value', () => {
+    const result = deser.toBigInt.deserialize(['not a bigint']);
+
+    expect(result).toStrictEqual(Failure.raise("Cannot convert 'not a bigint' to BigInt"));
   });
 });
