09266232c5eac3fbbdadafe2d7eb20b217caa73a
Benoit Lemoine <blemoine@xebia.fr>
2020-03-01 16:54:50 -0500

    chore(jest): configure jest to look only at src

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  globalSetup: "<rootDir>/test/postgres-up.ts",
-  globalTeardown: "<rootDir>/test/postgres-down.ts",
+  globalSetup: '<rootDir>/test/postgres-up.ts',
+  globalTeardown: '<rootDir>/test/postgres-down.ts',
+  testMatch: ['<rootDir>/src/**/*.(spec|test).(t|j)s'],
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello');
+import { ConnectionIO } from './connection/ConnectionIO';
+import { sql } from './query/sql-parser';
+import { Result, Success, Failure } from './result/Result';
+import { deser, namedDeser, NamedSqlDeserializer, PositionSqlDeserializer } from './serde/SqlDeserializer';
+
+export {
+  ConnectionIO,
+  sql,
+  Result,
+  Success,
+  Failure,
+  deser,
+  namedDeser,
+  NamedSqlDeserializer,
+  PositionSqlDeserializer,
+};
