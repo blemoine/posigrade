42e6862d7c5f8128b878462e3241864c416b8bc3
Benoit Lemoine <blemoine@xebia.fr>
2020-03-02 19:50:48 -0500

    feat: add some default export

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { ConnectionIO } from './connection/ConnectionIO';
 import { sql, sqlFrag } from './query/sql-parser';
 import { Result, Success, Failure } from './result/Result';
-import { deser, namedDeser, NamedSqlDeserializer, PositionSqlDeserializer } from './serde/SqlDeserializer';
+import {
+  deser,
+  namedDeser,
+  NamedSqlDeserializer,
+  PositionSqlDeserializer,
+  SqlDeserializer,
+  sequenceDeserRecord,
+  sequenceDeser,
+} from './serde/SqlDeserializer';
+import { SqlQuery } from './query/sql-query';
 
 export {
   ConnectionIO,
@@ -12,6 +21,10 @@ export {
   Failure,
   deser,
   namedDeser,
+  SqlDeserializer,
+  sequenceDeserRecord,
+  sequenceDeser,
   NamedSqlDeserializer,
   PositionSqlDeserializer,
+  SqlQuery,
 };
