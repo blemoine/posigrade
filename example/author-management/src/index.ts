b569783da2edad31e57ce7b5e9118021012af5f0
Benoit Lemoine <blemoine@xebia.fr>
2020-03-08 16:44:23 -0400

    refactor: remove example

--- example/author-management/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { router } from './router';
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use('/', router);
-
-app.listen(port, () => console.log(`Listening on port ${port}!`));
